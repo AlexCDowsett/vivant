0739470df593ad630308b25193e13eabf5ee8556
BuildTools <alexcdowsett@gmail.com>
2021-11-04 15:40:21 +0000

    Update to Python File

--- Python/sqlaccess.py
+++ Python/sqlaccess.py
@@ -1,70 +1,98 @@
 import pymysql
+import uuid
+import pickle
+import time
 
 # Configuration
+
 endpoint = 'vivant-db.c04gl03xq7f6.eu-west-2.rds.amazonaws.com'
 username = 'admin'
 password = '8Am5v9Ni'
 db_name = 'Vivant_DB'
 
+door_name = 'Test Door'
+
 conn = pymysql.connect(host=endpoint, user=username, passwd=password, db=db_name)
 
+def log(statement):
+    '''This function simply prints a string with the date and time as a prefix.'''
+    print('[{}] {}'.format(time.strftime('%d/%m/%y|%H:%M:%S'), statement)) # Prints message to the console.
+
+
+def main():
+    door = Door(door_name)
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM Doors')
+    rows = c.fetchall()
+    for row in rows:
+        print("{0} {1}".format(row[0], row[1]))
+    c.close()
+    conn.close()
 
 
 class Door:
     def __init__(self, door_name):
-        conn.begin()
+        self.name = door_name
         c = conn.cursor()
         
-        c.execute('INSERT INTO Doors (nickname) VALUES (%s);', (door_name))
-        conn.commit()
+        try: 
+            fd = open('uuid.data', 'rb')
+            self.uuid = pickle.load(fd)
+            c.execute("SELECT * FROM Doors WHERE door_uuid=%s;", (self.uuid))
 
-        c.execute('SELECT MAX(door_id) FROM Doors')
-        self.id = int(c.fetchone()[0])
-        self.name = door_name
+            if c.fetchone() == None:
+                c.execute('INSERT INTO Doors (door_uuid, nickname) VALUES (%s, %s);', (str(self.uuid), self.name))
+                conn.commit()
+                log(self.name + ' was missing from the database and has now been added.')
+
+            else:
+                log(self.name + ' loaded.')
+            
+        except FileNotFoundError:
+            log('New door detected.')
+            fw = open('uuid.data', 'wb')
+            self.uuid = uuid.uuid4()
+            pickle.dump(self.uuid, fw)
+            fw.close()
+
+            c.execute('INSERT INTO Doors (door_uuid, nickname) VALUES (%s, %s);', (str(self.uuid), self.name))
+            conn.commit()
 
         c.close()
-        conn.close()
         
 
     def __del__(self):
-        conn.begin()
         c = conn.cursor()
 
-        c.execute("DELETE FROM Doors WHERE door_id='%s';", (self.id))
-        c.commit()
+        c.execute("DELETE FROM Doors WHERE door_uuid=%s;", (self.uuid))
+        conn.commit()
         
         c.close()
-        conn.close()
+
         
     def ring():
+        c = conn.cursor()
+
+        c.execute('INSERT INTO RingLog (door_uuid) VALUES (%s);', (self.uuid))
+        conn.commit()
+
+        c.execute('SELECT MAX(ring_id) FROM RingLog WHERE door_uuid=%s;', (self.uuid))
+        temp = c.fetchone()[0]
+
+        c.execute('INSERT INTO TempUpdate (door_uuid, ring_id) VALUES (%s);', (self.uuid, temp))
+        conn.commit()
+
+        c.close()
+
+        # --> INSERT HERE CODE WHEN DOORBELL IS RUNG
+
+    def unlock():
         ''
 
+    def lock();
+        ''
+        
 
-front_door = Door('Front Door')
-back_door = Door('Back Door')
-
-conn.begin()
-c = conn.cursor()
-c.execute('SELECT * FROM LockLog')
-
-rows = c.fetchall()
-for row in rows:
-    print("{0} {1} {2}".format(row[0], row[1], row[2]))
-
-c.close()
-conn.close()
-
-del front_door, back_door
-
-conn.begin()
-c = conn.cursor()
-c.execute('SELECT * FROM LockLog')
-
-rows = c.fetchall()
-for row in rows:
-    print("{0} {1}".format(row[0], row[1]))
-
-c.close()
-conn.close()
-
-handler()
+if __name__ == '__main__': # Only runs the following code if the program is the main program and is not imported into another.
+    main() # Calls the main functio
